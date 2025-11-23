16aeeeec71fca86e8e77b6e433d5af47accc0145
Grumbee <ptwokay1008@gmail.com>
2025-11-23 23:53:38 +0100

    change icons

--- app/src/main/java/org/jellyfin/androidtv/ui/jellyseerr/JellyseerrFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/jellyseerr/JellyseerrFragment.kt
@@ -3,11 +3,11 @@
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.SoundEffectConstants
 import android.view.ViewGroup
+import android.net.Uri
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.background
